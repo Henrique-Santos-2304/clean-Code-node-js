fed469530170f270e089eb975d895be56ed40ba9
Henrique <henrique.multitech@gmail.com>
2022-04-09 11:42:40 -0300

    test: ensure SignUpnController return error with no param passwordConfirmatio

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -45,18 +45,20 @@ describe("Signup Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
-  // it("Should return 400 if no email is provided", () => {
-  //   const sut = new SignUpController();
-  //   const httpRequest = {
-  //     body: {
-  //       name: "john_do_name",
-  //       password: "john_doe",
-  //       passwordConfirmation: "john_doe",
-  //     },
-  //   };
+  it("Should return 400 if no password confimation is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "john_do_name",
+        email: "john_doe@email.com",
+        password: "john_doe",
+      },
+    };
 
-  //   const httpResponse = sut.handle(httpRequest);
-  //   expect(httpResponse.statusCode).toBe(400);
-  //   expect(httpResponse.body).toEqual(new MissingParamError("email"));
-  // });
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new MissingParamError("passwordConfirmation")
+    );
+  });
 });
