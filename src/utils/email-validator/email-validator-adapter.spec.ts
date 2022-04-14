2a72ec385d852a801674ebf5f277ac318ae2c3a1
Henrique <henrique.multitech@gmail.com>
2022-04-14 01:11:00 -0300

    test: ensure EmailValidator return true if validator returns true

--- src/utils/email-validator/email-validator-adapter.spec.ts
+++ src/utils/email-validator/email-validator-adapter.spec.ts
@@ -1,10 +1,22 @@
 import { EmailValidatorAdapter } from "./emial-validator";
+import validator from "validator";
+
+jest.mock("validator", () => ({
+  isEmail: (): boolean => true,
+}));
 
 describe("Email Validator Adapter", () => {
   it("Should return false if validator return false", () => {
     const sut = new EmailValidatorAdapter();
-    const isValid = sut.isValid("invalid_email@email.com");
+    jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
+    const isValid = sut.isValid("invalid_email");
 
     expect(isValid).toBe(false);
+  });
+  it("Should return true if validator return true", () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid("valid_email@email.com");
+
+    expect(isValid).toBe(true);
   });
 });
