ed86ca8227f80cb97c768d479085d68c6620bc22
Henrique <henrique.multitech@gmail.com>
2022-04-14 01:59:46 -0300

    test: ensure DbAddAccountThrowsIfEncrypter throws

--- src/data/useCases/add-account/de-add-account.spec.ts
+++ src/data/useCases/add-account/de-add-account.spec.ts
@@ -36,4 +36,16 @@ describe("DbAccount UseCase", () => {
     sut.add(accountData);
     expect(encryptSpy).toHaveBeenCalledWith("valid_password");
   });
+  it("Should throw if encrypted throws", () => {
+    const { sut, encrypterStub } = makeSut();
+    jest.spyOn(encrypterStub, "encrypt").mockRejectedValueOnce(new Error());
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password",
+    };
+
+    const promise = sut.add(accountData);
+    expect(promise).rejects.toThrow();
+  });
 });
