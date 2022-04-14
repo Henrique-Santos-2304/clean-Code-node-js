45f18a5c28de3f01c75f9e77f7fe8da4b954de78
Henrique <henrique.multitech@gmail.com>
2022-04-14 01:48:39 -0300

    refactor: move to sut and encrypter to a factory

--- src/data/useCases/add-account/de-add-account.spec.ts
+++ src/data/useCases/add-account/de-add-account.spec.ts
@@ -1,15 +1,27 @@
 import { IEncrypter } from "src/data/protocols/encrypter";
 import { DbAddAccount } from "./db-add-account";
 
+type SutTypes = {
+  sut: DbAddAccount;
+  encrypterStub: IEncrypter;
+};
+
+const makeSut = (): SutTypes => {
+  class EncrypterStub implements IEncrypter {
+    async encrypt() {
+      return "hashed_password";
+    }
+  }
+
+  const encrypterStub = new EncrypterStub();
+  const sut = new DbAddAccount(encrypterStub);
+
+  return { sut, encrypterStub };
+};
+
 describe("DbAccount UseCase", () => {
   it("Should call encrypted with correct password", () => {
-    class EncrypterStub implements IEncrypter {
-      async encrypt() {
-        return "hashed_password";
-      }
-    }
-    const encrypterStub = new EncrypterStub();
-    const sut = new DbAddAccount(encrypterStub);
+    const { sut, encrypterStub } = makeSut();
     const encryptSpy = jest.spyOn(encrypterStub, "encrypt");
     const accountData = {
       name: "valid_name",
